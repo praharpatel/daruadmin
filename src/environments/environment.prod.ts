2466923f622f41b3ea9a9d2c825bab02f9e34675
fjpimienta <fjpimienta@gmail.com>
2023-07-08 18:39:13 -0600

    Admin: Para Publicacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  backend: 'https://api.daru.mx/graphql',
-  backendWs: 'wss://api.daru.mx/graphql',
+  backend: 'http://189.253.186.95:8080/graphql',
+  backendWs: 'ws://189.253.186.95:8080/graphql',
   stripePublicKey: 'pk_test_51JIL74J3AWJEzlXbrPLgiyb1RdjtNR4Raz49wGw3CsU8YMes5ZhSw6Z7Qx2TCabBD5gYcVyV4cJSwVJUXWTleF1O00XRREKbZk',
-  upload: 'https://api.daru.mx/upload',
-  uploadsUrl: 'https://api.daru.mx/uploads',
+  upload: 'http://189.253.186.95:8080/uploads',
+  uploadsUrl: 'http://189.253.186.95:8080/uploads',
 
   defaultauth: 'fackbackend',
   firebaseConfig: {
