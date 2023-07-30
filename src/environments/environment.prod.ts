8e3ce9c1a1faa48c48265ebf39cae6d44f66368d
fjpimienta <fjpimienta@gmail.com>
2023-07-30 14:52:47 -0600

    Admin: Configuraciones API .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  backend: 'https://api.daru.mx:3001/graphql',
-  backendWs: 'wss://api.daru.mx:3001/graphql',
+  backend: 'https://api.daru.mx:3000/graphql',
+  backendWs: 'wss://api.daru.mx:3000/graphql',
   stripePublicKey: 'pk_test_51JIL74J3AWJEzlXbrPLgiyb1RdjtNR4Raz49wGw3CsU8YMes5ZhSw6Z7Qx2TCabBD5gYcVyV4cJSwVJUXWTleF1O00XRREKbZk',
-  upload: 'https://api.daru.mx:3001/upload',
-  uploadsUrl: 'https://api.daru.mx:3001/uploads',
+  upload: 'https://api.daru.mx:3000/upload',
+  uploadsUrl: 'https://api.daru.mx:3000/uploads',
 
   defaultauth: 'fackbackend',
   firebaseConfig: {
