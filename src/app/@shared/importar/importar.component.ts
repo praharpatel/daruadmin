496b133af94dd1fb8dcec124c5fa0c057876a3dd
fjpimienta <fjpimienta@gmail.com>
2023-09-07 11:50:43 -0600

    Admin: Optimizacion y No tomar almacenes que no estan en el catalogo.

--- src/app/@shared/importar/importar.component.ts
+++ src/app/@shared/importar/importar.component.ts
@@ -633,23 +633,19 @@ export class ImportarComponent implements OnInit {
   getAlmacenCant(branch): BranchOffices {
     const almacen = new BranchOffices();
     const almacenEstado = this.getCtAlmacenes(branch.almacen.key);
-    almacen.id = almacenEstado.id;
-    almacen.name = almacenEstado.Sucursal;
-    almacen.estado = almacenEstado.Estado;
-    almacen.cp = almacenEstado.CP;
-    almacen.latitud = almacenEstado.latitud;
-    almacen.longitud = almacenEstado.longitud;
-    almacen.cantidad = branch.almacen.value;
+    if (almacenEstado) {
+      const { id, name, estado, cp, latitud, longitud } = almacenEstado;
+      Object.assign(almacen, { id, name, estado, cp, latitud, longitud, cantidad: branch.almacen.value });
+    }
     return almacen;
   }
 
-  getCtAlmacenes(id: string): any {
-    const almacen = this.ctAlmacenes.filter(almacen => almacen.id === id);
-    if (almacen.length > 0) {
-      const sucursal = almacen.map(element => element);
-      return sucursal[0];
+  getCtAlmacenes(id: string): BranchOffices | null {
+    const almacenEstado = this.ctAlmacenes.find(almacen => almacen.id === id);
+    if (almacenEstado) {
+      return almacenEstado;
     }
-    return '';
+    return null;
   }
 
   getFechas(fecha: Date) {
@@ -1175,16 +1171,10 @@ export class ImportarComponent implements OnInit {
         disponible = 0;
         salePrice = 0;
         if (item.almacenes.length > 0) {
-          const branchOfficesCt: BranchOffices[] = [];
           let featured = false;
-          for (const element of item.almacenes) {
-            const almacen = this.getAlmacenCant(element);
-            if (almacen.cantidad >= this.stockMinimo) {
-              disponible = almacen.cantidad;
-              branchOfficesCt.push(almacen);
-            }
-          }
-          // if (disponible >= this.stockMinimo) {                         // Si hay mas de 10 elementos disponibles
+          const branchOfficesCt = item.almacenes
+            .map(element => this.getAlmacenCant(element))
+            .filter(almacen => almacen.cantidad >= this.stockMinimo && almacen.name !== '');
           if (branchOfficesCt.length > 0) {                         // Si hay mas de 10 elementos disponibles
             // Si hay promociones en los almacenes ocupa el primero y asigna el total de disponibilidad
             if (item.almacenes[0].promociones[0]) {
