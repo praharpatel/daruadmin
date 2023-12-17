295d31c436c7b26ba036e835a8ed90cbe8668291
fjpimienta <fjpimienta@gmail.com>
2023-12-16 22:05:35 -0600

    Admin: CRUD Catalogo de Cupones.

--- src/app/@graphql/operations/fragment/cupon.ts
+++ src/app/@graphql/operations/fragment/cupon.ts
@@ -3,14 +3,11 @@ import gql from 'graphql-tag';
 export const CUPON_FRAGMENT = gql`
   fragment CuponObject on Cupon {
     id
+    cupon
     description
-    slug
-    order
+    typeDiscount
+    amountDiscount
+    minimumPurchase
     active
-    suppliersCat {
-      idProveedor
-      name
-      slug
-    }
   }
 `;
